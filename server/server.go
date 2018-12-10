b3dc7c0fe26a9cfdda0b300d2f9d808ab605e787
Marco Argentieri <marco@vulpem.com>
2018-12-10 23:22:06 +0100

    Reputation adapter

--- server/server.go
+++ server/server.go
@@ -1,27 +1,3 @@
-/*
- *
- * Copyright 2015 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
-//go:generate protoc -I ../routeguide --go_out=plugins=grpc:../routeguide ../routeguide/route_guide.proto
-
-// Package main implements a simple gRPC server that demonstrates how to use gRPC-Go libraries
-// to perform unary, client streaming, server streaming and full duplex RPCs.
-//
-// It implements the route guide service whose definition can be found in routeguide/route_guide.proto.
 package server
 
 import (
@@ -36,8 +12,8 @@ import (
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/testdata"
 
-	"github.com/singnet/reputation/adapter/database"
-	pb "github.com/singnet/reputation/adapter/resources/protos"
+	"github.com/singnet/reputation-adapter/database"
+	pb "github.com/singnet/reputation-adapter/resources/protos"
 )
 
 var (
